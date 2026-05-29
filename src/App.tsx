e9a9325a14e5aa36a028b13dc302815ec45a73d4
Ravaroel10 <rafa100609@gmail.com>
2026-05-29 15:43:02 +0700

    Update API URL to remove protocol from fetch call

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const handleCalculate = async () => {
     setIsCalculating(true)           // ✅ bukan setLoading
 
   try {
-    const response = await fetch(`https://backendzether-production.up.railway.app/calculate?n=${n}`);
+    const response = await fetch(`backendzether-production.up.railway.app/calculate?n=${n}`);
     const data = await response.json();
 
 const formattedData = Array.isArray(data.convergence_data)
