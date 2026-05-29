0d9d612a2a27f0f10e5404eda949836ee9b00a21
Ravaroel10 <rafa100609@gmail.com>
2026-05-29 15:45:23 +0700

    Refactor API URL for fetch request in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,12 @@ const handleCalculate = async () => {
     setIsCalculating(true)           // ✅ bukan setLoading
 
   try {
-    const response = await fetch(`backendzether-production.up.railway.app/calculate?n=${n}`);
+    const API_URL =
+  "https://backendzether-production.up.railway.app";
+
+const response = await fetch(
+  `${API_URL}/calculate?n=${n}`
+);
     const data = await response.json();
 
 const formattedData = Array.isArray(data.convergence_data)
